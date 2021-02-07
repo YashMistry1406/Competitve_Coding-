0d79e7cdc8f46b90ae1250e4188f88290aae193c
YashMistry1406 <yashmistry2000@gmail.com>
2021-02-07 12:17:29 +0530

    cp

--- Maximise_Function.py
+++ Maximise_Function.py
@@ -1,27 +1,27 @@
-def partition(arr,low,high):
-    i = ( low-1 )         # index of smaller element 
-    pivot = arr[high]     # pivot 
-    for j in range(low , high): 
-        if   arr[j] < pivot: 
-            i = i+1
-            arr[i],arr[j] = arr[j],arr[i] 
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
-    return ( i+1 )
-def quickSort(arr,low,high): 
-    if low < high: 
-        pi = partition(arr,low,high) 
-        quickSort(arr, low, pi-1) 
-        quickSort(arr, pi+1, high) 
-
-
-for i in range(int(input())):
-    temp=int(input())
-    S=list(map(int,input().split()))
-
-    n=len(S)
-    quickSort(S,0,n-1)
-    mid=n//2
-    x=S[0]
-    y=S[mid]
-    z=S[n-1]
-    print(abs(x-y)+abs(y-z)+abs(z-x))
+try:
+    def partition(arr,low,high):
+        i = ( low-1 )         # index of smaller element 
+        pivot = arr[high]     # pivot 
+        for j in range(low , high): 
+            if   arr[j] < pivot: 
+                i = i+1
+                arr[i],arr[j] = arr[j],arr[i] 
+        arr[i+1],arr[high] = arr[high],arr[i+1] 
+        return ( i+1 )
+    def quickSort(arr,low,high): 
+        if low < high: 
+            pi = partition(arr,low,high) 
+            quickSort(arr, low, pi-1) 
+            quickSort(arr, pi+1, high) 
+    for i in range(int(input())):
+        temp=int(input())
+        S=list(map(int,input().split()))
+        n=len(S)
+        quickSort(S,0,n-1)
+        mid=n//2
+        x=S[0]
+        y=S[mid]
+        z=S[n-1]
+        print(abs(x-y)+abs(y-z)+abs(z-x))
+except Exception as e:
+    print(e)
